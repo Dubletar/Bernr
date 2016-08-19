bf35747eb6f52fbe93b0ec7e864da44025ab1d70
dubletar <dubletar@gmail.com>
2016-08-19 14:37:52 -0500

    Save.

--- src/UserBundle/Controller/UserController.php
+++ src/UserBundle/Controller/UserController.php
@@ -25,7 +25,7 @@ class UserController extends AbstractController
         $username = $request->request->get('username');
         $password = md5($request->request->get('password'));
         $isApp = $request->request->get('app');
-        return $this->createJmsResponse($isApp);
+        return $this->createJmsResponse($request->request->all());
         
         // Set up return data container.
         $data = array();
