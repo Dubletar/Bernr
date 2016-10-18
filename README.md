fcdc9826ed199c8c56682fbfa7a465bc952172f9
adamCaxy <avelma@caxy.com>
2016-10-18 13:13:44 -0500

    Upgrade to 3.0

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 Bernr
-====
+=====
+
+A Symfony project created on October 18, 2016, 11:52 am.
--- cacheclear.sh
+++ cacheclear.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-./app/console assets:install --symlink --relative
-./app/console cache:clear --env=prod --no-debug 
-./app/console cache:clear --env=dev
-./app/console assetic:dump --env=prod --no-debug
-./app/console assetic:dump --env=dev
+./bin/console assets:install --symlink --relative
+./bin/console cache:clear --env=prod --no-debug
+./bin/console cache:clear --env=dev
+./bin/console assetic:dump --env=prod --no-debug
+./bin/console assetic:dump --env=dev
 
